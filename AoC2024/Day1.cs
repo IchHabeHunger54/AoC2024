d51dbb6ee9165a45853a802e4f74b573251a2667
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-12-01 12:36:40 +0100

    day 1 task 1

---
+++ AoC2024/Day1.cs
@@ -0,0 +1,19 @@
+﻿namespace AoC2024;
+
+public class Day1() : Day(1)
+{
+    protected override int TaskOne(List<string> lines)
+    {
+        List<string[]> data = lines.Select(x => x.Split("   ")).ToList();
+        List<string> left = data.Select(x => x[0]).ToList();
+        List<string> right = data.Select(x => x[1]).ToList();
+        left.Sort(); 
+        right.Sort();
+        return left.Select((t, i) => Math.Abs(int.Parse(t) - int.Parse(right[i]))).Sum();
+    }
+
+    protected override int TaskTwo(List<string> lines)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# AoC2024
+
+This is my solution for [Advent of Code 2024](https://adventofcode.com/2024), written in C#.
+
+Inputs go into the git-ignored `input` directory.
+
+All code in this repository is licensed under the MIT license.
