1affdfd9a70b7b6b7576cb55139b0d97112d469e
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-12-01 12:48:01 +0100

    day 1 task 2

--- AoC2024/Day1.cs
+++ AoC2024/Day1.cs
@@ -5,15 +5,18 @@ public class Day1() : Day(1)
     protected override int TaskOne(List<string> lines)
     {
         List<string[]> data = lines.Select(x => x.Split("   ")).ToList();
-        List<string> left = data.Select(x => x[0]).ToList();
-        List<string> right = data.Select(x => x[1]).ToList();
+        List<int> left = data.Select(x => x[0]).Select(int.Parse).ToList();
+        List<int> right = data.Select(x => x[1]).Select(int.Parse).ToList();
         left.Sort(); 
         right.Sort();
-        return left.Select((t, i) => Math.Abs(int.Parse(t) - int.Parse(right[i]))).Sum();
+        return left.Select((t, i) => Math.Abs(t - right[i])).Sum();
     }
 
     protected override int TaskTwo(List<string> lines)
     {
-        throw new NotImplementedException();
+        List<string[]> data = lines.Select(x => x.Split("   ")).ToList();
+        List<int> left = data.Select(x => x[0]).Select(int.Parse).ToList();
+        List<int> right = data.Select(x => x[1]).Select(int.Parse).ToList();
+        return left.Select(x => right.Where(y => y == x).Sum()).Sum();
     }
 }
