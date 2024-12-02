bece6923f2bcc72cc9128cb90ee387a44c80f9e6
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-12-02 16:40:52 +0100

    day 2 task 1

--- AoC2024/Day.cs
+++ AoC2024/Day.cs
@@ -9,20 +9,20 @@ public abstract class Day(int dayNumber)
 
     private static Day GetDay()
     {
-        return new Day1();
+        return new Day2();
     }
 
     private static List<string> ReadLines(string path)
     {
-        // We need to be relative to the bin/Debug/net9.0 directory to throw our files into the , so here we go
-        string absolutePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../input" + path;
+        // We need to be relative to the bin/Debug/net9.0 directory to throw our files into the <project_root>/input directory, so here we go
+        string absolutePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../input/" + path;
         return File.ReadAllLines(absolutePath).ToList();
     }
 
     private void Run()
     {
-        Console.WriteLine("Day " + dayNumber + ", Task 1: " + TaskOne(ReadLines("./day_" + dayNumber + "_task_1.txt")));
-        Console.WriteLine("Day " + dayNumber + ", Task 2: " + TaskTwo(ReadLines("./day_" + dayNumber + "_task_2.txt")));
+        Console.WriteLine("Day " + dayNumber + ", Task 1: " + TaskOne(ReadLines("day_" + dayNumber + "_task_1.txt")));
+        Console.WriteLine("Day " + dayNumber + ", Task 2: " + TaskTwo(ReadLines("day_" + dayNumber + "_task_2.txt")));
     }
 
     protected abstract int TaskOne(List<string> lines);
---
+++ AoC2024/Day2.cs
@@ -0,0 +1,25 @@
+﻿namespace AoC2024;
+
+public class Day2() : Day(2)
+{
+    protected override int TaskOne(List<string> lines)
+    {
+        List<List<int>> grid = lines.Select(x => x.Split(' ').Select(int.Parse).ToList()).ToList();
+        bool[] result = new bool[grid.Count];
+        for (int i = 0; i < grid.Count; i++)
+        {
+            List<int> row = grid[i];
+            result[i] = row.SequenceEqual(row.OrderBy(x => x)) || row.SequenceEqual(row.OrderByDescending(x => x));
+            for (int j = 0; j < row.Count - 1; j++)
+            {
+                result[i] &= Math.Abs(row[j] - row[j + 1]) is > 0 and <= 3;
+            }
+        }
+        return result.Select(x => x ? 1 : 0).Sum();
+    }
+
+    protected override int TaskTwo(List<string> lines)
+    {
+        throw new NotImplementedException();
+    }
+}
