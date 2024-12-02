471ca8834087f17b91f458e0ee6f56f687a42f69
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-12-02 16:56:22 +0100

    day 2 task 2

--- AoC2024/Day2.cs
+++ AoC2024/Day2.cs
@@ -4,22 +4,41 @@ public class Day2() : Day(2)
 {
     protected override int TaskOne(List<string> lines)
     {
-        List<List<int>> grid = lines.Select(x => x.Split(' ').Select(int.Parse).ToList()).ToList();
-        bool[] result = new bool[grid.Count];
-        for (int i = 0; i < grid.Count; i++)
-        {
-            List<int> row = grid[i];
-            result[i] = row.SequenceEqual(row.OrderBy(x => x)) || row.SequenceEqual(row.OrderByDescending(x => x));
-            for (int j = 0; j < row.Count - 1; j++)
-            {
-                result[i] &= Math.Abs(row[j] - row[j + 1]) is > 0 and <= 3;
-            }
-        }
-        return result.Select(x => x ? 1 : 0).Sum();
+        return lines.Select(x => x.Split(' ').Select(int.Parse).ToList()).Sum(t => CheckRow(t) ? 1 : 0);
     }
 
     protected override int TaskTwo(List<string> lines)
     {
-        throw new NotImplementedException();
+        List<List<int>> grid = lines.Select(x => x.Split(' ').Select(int.Parse).ToList()).ToList();
+        int result = 0;
+        foreach (List<int> row in grid)
+        {
+            if (CheckRow(row))
+            {
+                result++;
+            }
+            else
+            {
+                int problems = 0;
+                for (int i = 0; i < row.Count; i++)
+                {
+                    List<int> copy = row.ToList();
+                    copy.RemoveAt(i);
+                    problems += CheckRow(copy) ? 1 : 0;
+                }
+                result += problems > 0 ? 1 : 0;
+            }
+        }
+        return result;
+    }
+
+    private static bool CheckRow(List<int> row)
+    {
+        bool result = row.SequenceEqual(row.OrderBy(x => x)) || row.SequenceEqual(row.OrderByDescending(x => x));
+        for (int j = 0; j < row.Count - 1; j++)
+        {
+            result &= Math.Abs(row[j] - row[j + 1]) is > 0 and <= 3;
+        }
+        return result;
     }
 }
