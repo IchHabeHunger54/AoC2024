73d4fcc77411f923f649b0b05ba7c4d4f1a33a90
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-12-03 12:39:20 +0100

    day 3 task 2

--- AoC2024/Day3.cs
+++ AoC2024/Day3.cs
@@ -13,7 +13,7 @@ public class Day3() : Day(3)
             switch (stage)
             {
                 case Stage.Opening:
-                    if (line.Length - i > 3 && line[i] == 'm' && line[i + 1] == 'u' && line[i + 2] == 'l' && line[i + 3] == '(')
+                    if (line.Length - i >= 4 && line.Substring(i, 4).Equals("mul("))
                     {
                         stage = Stage.Comma;
                         i += 3;
@@ -60,7 +60,77 @@ public class Day3() : Day(3)
 
     protected override int TaskTwo(List<string> lines)
     {
-        throw new NotImplementedException();
+        string line = lines.Aggregate("", (current, s) => current + s);
+        int sum = 0;
+        Stage stage = Stage.Opening;
+        (string a, string b) currentPair = ("", "");
+        bool enabled = true;
+        for (int i = 0; i < line.Length; i++)
+        {
+            switch (line.Length - i)
+            {
+                case >= 4 when line.Substring(i, 4).Equals("do()"):
+                    enabled = true;
+                    break;
+                case >= 7 when line.Substring(i, 7).Equals("don't()"):
+                    enabled = false;
+                    currentPair = ("", "");
+                    stage = Stage.Opening;
+                    break;
+                default:
+                {
+                    if (enabled)
+                    {
+                        switch (stage)
+                        {
+                            case Stage.Opening:
+                                if (line.Length - i >= 4 && line.Substring(i, 4).Equals("mul("))
+                                {
+                                    stage = Stage.Comma;
+                                    i += 3;
+                                }
+                                break;
+                            case Stage.Comma:
+                                switch (line[i])
+                                {
+                                    case >= '0' and <= '9':
+                                        currentPair.a += line[i];
+                                        break;
+                                    case ',':
+                                        stage = Stage.Closing;
+                                        break;
+                                    default:
+                                        currentPair = ("", "");
+                                        stage = Stage.Opening;
+                                        break;
+                                }
+                                break;
+                            case Stage.Closing:
+                                switch (line[i])
+                                {
+                                    case >= '0' and <= '9':
+                                        currentPair.b += line[i];
+                                        break;
+                                    case ')':
+                                        sum += int.Parse(currentPair.a) * int.Parse(currentPair.b);
+                                        currentPair = ("", "");
+                                        stage = Stage.Opening;
+                                        break;
+                                    default:
+                                        currentPair = ("", "");
+                                        stage = Stage.Opening;
+                                        break;
+                                }
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException(nameof(lines));
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        return sum;
     }
 
     private enum Stage
