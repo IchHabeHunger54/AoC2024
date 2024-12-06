d1baee19ca6d723f15cdfd6427f4629e2d6377a0
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-12-06 21:37:53 +0100

    day 6 task 2 but even faster

--- AoC2024/Day6.cs
+++ AoC2024/Day6.cs
@@ -4,74 +4,20 @@ public class Day6() : Day(6)
 {
     protected override int TaskOne(List<string> lines)
     {
-        int sizeX = lines[0].Length;
-        int sizeY = lines.Count;
-        (int x, int y) guard = GetGuard(lines);
-        Direction direction = Direction.Up;
-        HashSet<(int x, int y)> result = [guard];
-        while (guard is { x: > -1, y: > -1 } && guard.x < sizeX && guard.y < sizeY)
-        {
-            switch (direction)
-            {
-                case Direction.Up:
-                    if (guard.y > 0 && lines[guard.y - 1][guard.x] == '#')
-                    {
-                        direction = Direction.Right;
-                    }
-                    else
-                    {
-                        guard = (guard.x, guard.y - 1);
-                        result.Add(guard);
-                    }
-                    break;
-                case Direction.Down:
-                    if (guard.y < sizeY - 1 && lines[guard.y + 1][guard.x] == '#')
-                    {
-                        direction = Direction.Left;
-                    }
-                    else
-                    {
-                        guard = (guard.x, guard.y + 1);
-                        result.Add(guard);
-                    }
-                    break;
-                case Direction.Left:
-                    if (guard.x > 0 && lines[guard.y][guard.x - 1] == '#')
-                    {
-                        direction = Direction.Up;
-                    }
-                    else
-                    {
-                        guard = (guard.x - 1, guard.y);
-                        result.Add(guard);
-                    }
-                    break;
-                case Direction.Right:
-                    if (guard.x < sizeX - 1 && lines[guard.y][guard.x + 1] == '#')
-                    {
-                        direction = Direction.Down;
-                    }
-                    else
-                    {
-                        guard = (guard.x + 1, guard.y);
-                        result.Add(guard);
-                    }
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(lines));
-            }
-        }
-        return result.Count - 1;
+        return GetPositions(lines).ToList().Count - 1;
     }
 
     protected override int TaskTwo(List<string> lines)
     {
+        List<(int x, int y)> positions = GetPositions(lines).ToList();
         (int x, int y) guard = GetGuard(lines);
+        positions.Remove(guard);
         int result = 0;
         for (int i = 0; i < lines.Count; i++)
         {
             for (int j = 0; j < lines[i].Length; j++)
             {
+                if (!positions.Contains((j, i))) continue;
                 if (lines[i][j] != '.') continue;
                 lines[i] = lines[i][..j] + 'O' + lines[i][(j + 1)..];
                 if (IsLoop(lines, guard.x, guard.y))
@@ -160,6 +106,68 @@ public class Day6() : Day(6)
         return false;
     }
 
+    private static IEnumerable<(int x, int y)> GetPositions(List<string> lines)
+    {
+        int sizeX = lines[0].Length;
+        int sizeY = lines.Count;
+        (int x, int y) guard = GetGuard(lines);
+        Direction direction = Direction.Up;
+        HashSet<(int x, int y)> result = [guard];
+        while (guard is { x: > -1, y: > -1 } && guard.x < sizeX && guard.y < sizeY)
+        {
+            switch (direction)
+            {
+                case Direction.Up:
+                    if (guard.y > 0 && lines[guard.y - 1][guard.x] == '#')
+                    {
+                        direction = Direction.Right;
+                    }
+                    else
+                    {
+                        guard = (guard.x, guard.y - 1);
+                        result.Add(guard);
+                    }
+                    break;
+                case Direction.Down:
+                    if (guard.y < sizeY - 1 && lines[guard.y + 1][guard.x] == '#')
+                    {
+                        direction = Direction.Left;
+                    }
+                    else
+                    {
+                        guard = (guard.x, guard.y + 1);
+                        result.Add(guard);
+                    }
+                    break;
+                case Direction.Left:
+                    if (guard.x > 0 && lines[guard.y][guard.x - 1] == '#')
+                    {
+                        direction = Direction.Up;
+                    }
+                    else
+                    {
+                        guard = (guard.x - 1, guard.y);
+                        result.Add(guard);
+                    }
+                    break;
+                case Direction.Right:
+                    if (guard.x < sizeX - 1 && lines[guard.y][guard.x + 1] == '#')
+                    {
+                        direction = Direction.Down;
+                    }
+                    else
+                    {
+                        guard = (guard.x + 1, guard.y);
+                        result.Add(guard);
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(lines));
+            }
+        }
+        return result;
+    }
+    
     private record PositionAndDirection(int X, int Y, Direction Direction)
     {
         public virtual bool Equals(PositionAndDirection? other)
