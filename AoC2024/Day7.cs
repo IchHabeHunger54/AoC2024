4e149cb81387b3c846b71d6fe27da6ceecc07ea0
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-12-07 13:47:40 +0100

    day 7 task 2

--- AoC2024/Day7.cs
+++ AoC2024/Day7.cs
@@ -4,7 +4,31 @@ public class Day7() : Day(7)
 {
     protected override int TaskOne(List<string> lines)
     {
-        long result = (from line in lines select line.Split(" ") into tokens let left = long.Parse(tokens[0][..(tokens[0].Length - 1)]) let right = tokens[1..].Select(long.Parse).ToArray() where CanGiveResultOne(right[0], right[1..], left) select left).Sum();
+        long result = 0;
+        foreach (string line in lines)
+        {
+            string[] tokens = line.Split(" ");
+            long left = long.Parse(tokens[0][..(tokens[0].Length - 1)]);
+            long[] right = tokens[1..].Select(long.Parse).ToArray();
+            if (CanGiveResultOne(right[0], right[1..], left)) result += left;
+        }
+        if (result > int.MaxValue)
+        {
+            Console.WriteLine($"Long value result: {result}");
+        }
+        return (int) result;
+    }
+
+    protected override int TaskTwo(List<string> lines)
+    {
+        long result = 0;
+        foreach (string line in lines)
+        {
+            string[] tokens = line.Split(" ");
+            long left = long.Parse(tokens[0][..(tokens[0].Length - 1)]);
+            long[] right = tokens[1..].Select(long.Parse).ToArray();
+            if (CanGiveResultTwo(right[0], right[1..], left)) result += left;
+        }
         if (result > int.MaxValue)
         {
             Console.WriteLine($"Long value result: {result}");
@@ -19,8 +43,12 @@ public class Day7() : Day(7)
         return CanGiveResultOne(first + others[0], others[1..], result) || CanGiveResultOne(first * others[0], others[1..], result);
     }
 
-    protected override int TaskTwo(List<string> lines)
+    private static bool CanGiveResultTwo(long first, long[] others, long result)
     {
-        throw new NotImplementedException();
+        if (first > result) return false;
+        if (others.Length == 0) return first == result;
+        return CanGiveResultTwo(first + others[0], others[1..], result)
+               || CanGiveResultTwo(first * others[0], others[1..], result)
+               || CanGiveResultTwo(first * (long) Math.Pow(10, others[0].ToString().Length) + others[0], others[1..], result);
     }
 }
