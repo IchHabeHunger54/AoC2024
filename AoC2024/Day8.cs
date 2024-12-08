8c4d82a8145eae710397c2f51b737777f8faf38a
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-12-08 18:01:16 +0100

    day 8 task 2

--- AoC2024/Day8.cs
+++ AoC2024/Day8.cs
@@ -6,6 +6,80 @@ public class Day8() : Day(8)
     {
         int sizeX = lines[0].Length;
         int sizeY = lines.Count;
+        Dictionary<char, List<(int x, int y)>> dict = GetAntennae(lines);
+        HashSet<(int x, int y)> points = [];
+        foreach (List<(int x, int y)> list in dict.Keys.Select(c => dict[c]))
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                (int x, int y) current = list[i];
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    (int x, int y) next = list[j];
+                    int diffX = next.x - current.x;
+                    int diffY = next.y - current.y;
+                    int x = current.x - diffX;
+                    int y = current.y - diffY;
+                    if (x >= 0 && x < sizeX && y >= 0 && y < sizeY)
+                    {
+                        points.Add((x, y));
+                    }
+                    x = next.x + diffX;
+                    y = next.y + diffY;
+                    if (x >= 0 && x < sizeX && y >= 0 && y < sizeY)
+                    {
+                        points.Add((x, y));
+                    }
+                }
+            }
+        }
+        return points.Count;
+    }
+
+    protected override long TaskTwo(List<string> lines)
+    {
+        int sizeX = lines[0].Length;
+        int sizeY = lines.Count;
+        Dictionary<char, List<(int x, int y)>> dict = GetAntennae(lines);
+        HashSet<(int x, int y)> points = [];
+        foreach (List<(int x, int y)> list in dict.Keys.Select(c => dict[c]))
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                (int x, int y) current = list[i];
+                points.Add(current);
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    (int x, int y) next = list[j];
+                    points.Add(next);
+                    int diffX = next.x - current.x;
+                    int diffY = next.y - current.y;
+                    int x = current.x - diffX;
+                    int y = current.y - diffY;
+                    while (x >= 0 && x < sizeX && y >= 0 && y < sizeY)
+                    {
+                        points.Add((x, y));
+                        x -= diffX;
+                        y -= diffY;
+                    }
+                    x = next.x + diffX;
+                    y = next.y + diffY;
+                    while (x >= 0 && x < sizeX && y >= 0 && y < sizeY)
+                    {
+                        points.Add((x, y));
+                        x += diffX;
+                        y += diffY;
+                    }
+                }
+            }
+        }
+        return points.Count;
+    }
+
+    private static Dictionary<char, List<(int x, int y)>> GetAntennae(List<string> lines)
+    {
+        int sizeX = lines[0].Length;
+        int sizeY = lines.Count;
         Dictionary<char, List<(int x, int y)>> dict = new();
         for (int y = 0; y < sizeY; y++)
         {
@@ -20,37 +94,6 @@ public class Day8() : Day(8)
                 dict[c].Add((x, y));
             }
         }
-        HashSet<(int x, int y)> points = [];
-        foreach (List<(int x, int y)> list in dict.Keys.Select(c => dict[c]))
-        {
-            for (int i = 0; i < list.Count - 1; i++)
-            {
-                (int x, int y) current = list[i];
-                for (int j = i + 1; j < list.Count; j++)
-                {
-                    (int x, int y) next = list[j];
-                    int diffX = next.x - current.x;
-                    int diffY = next.y - current.y;
-                    int x1 = current.x - diffX;
-                    int x2 = next.x + diffX;
-                    int y1 = current.y - diffY;
-                    int y2 = next.y + diffY;
-                    if (!(x1 < 0 || x1 >= sizeX || y1 < 0 || y1 >= sizeY))
-                    {
-                        points.Add((x1, y1));
-                    }
-                    if (!(x2 < 0 || x2 >= sizeX || y2 < 0 || y2 >= sizeY))
-                    {
-                        points.Add((x2, y2));
-                    }
-                }
-            }
-        }
-        return points.Count;
-    }
-
-    protected override long TaskTwo(List<string> lines)
-    {
-        throw new NotImplementedException();
+        return dict;
     }
 }
