3f4d0b82295136f8ff26c4e71dc11cdd001b13a5
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-12-09 16:16:12 +0100

    day 9 task 2

--- AoC2024/Day9.cs
+++ AoC2024/Day9.cs
@@ -20,6 +20,7 @@ public class Day9() : Day(9)
         while (start <= end)
         {
             int? value = list[start];
+            start++;
             if (value != null)
             {
                 result.Add(value.Value);
@@ -35,13 +36,57 @@ public class Day9() : Day(9)
                 }
                 result.Add(toMove.Value);
             }
-            start++;
         }
         return result.Select((x, i) => x * i).Sum();
     }
 
     protected override long TaskTwo(List<string> lines)
     {
-        throw new NotImplementedException();
+        List<int> line = lines[0].Select(c => c - '0').ToList();
+        List<Block> list = [];
+        for (int i = 0; i < line.Count; i++)
+        {
+            int? value = i % 2 == 0 ? i / 2 : null;
+            if (line[i] > 0)
+            {
+                list.Add(new Block(line[i], value));
+            }
+        }
+        for (int i = list.Count - 1; i >= 0; i--)
+        {
+            Block block = list[i];
+            if (block.Value == null) continue;
+            for (int j = 0; j < i; j++)
+            {
+                Block newBlock = list[j];
+                if (newBlock.Value != null || newBlock.Size < block.Size) continue;
+                list.Insert(j, block);
+                int newSize = newBlock.Size - block.Size;
+                if (newSize > 0)
+                {
+                    list[j + 1] = new Block(newSize, null);
+                    i++;
+                }
+                else
+                {
+                    list.RemoveAt(j + 1);
+                }
+                list[i] = block with { Value = null };
+                break;
+            }
+        }
+        int index = 0;
+        long result = 0;
+        foreach (Block block in list)
+        {
+            for (int i = 0; i < block.Size; i++)
+            {
+                result += (block.Value ?? 0) * index;
+                index++;
+            }
+        }
+        return result;
     }
+
+    private record Block(int Size, int? Value);
 }
