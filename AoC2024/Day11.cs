557d2b1a309444f36286db25772f8711dc3d1839
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-12-11 16:53:39 +0100

    day 11 task 2 part 1

--- AoC2024/Day11.cs
+++ AoC2024/Day11.cs
@@ -4,34 +4,40 @@ public class Day11() : Day(11)
 {
     protected override long TaskOne(List<string> lines)
     {
-        List<long> numbers = lines[0].Split(' ').Select(long.Parse).ToList();
-        for (int i = 0; i < 25; i++)
-        {
-            for (int j = 0; j < numbers.Count; j++)
-            {
-                long number = numbers[j];
-                if (number == 0)
-                {
-                    numbers[j] = 1;
-                }
-                else if (number.ToString().Length % 2 == 0)
-                {
-                    string s = number.ToString();
-                    numbers[j] = long.Parse(s[..(s.Length / 2)]);
-                    numbers.Insert(j + 1, long.Parse(s[(s.Length / 2)..]));
-                    j++;
-                }
-                else
-                {
-                    numbers[j] *= 2024;
-                }
-            }
-        }
-        return numbers.Count;
+        return DoTask(lines, 25);
     }
 
     protected override long TaskTwo(List<string> lines)
     {
-        throw new NotImplementedException();
+        return DoTask(lines, 75);
+    }
+
+    private static long DoTask(List<string> lines, int runs)
+    {
+        List<long> numbers = lines[0].Split(' ').Select(long.Parse).ToList();
+        List<Thread> threads = [];
+        List<long> results = [];
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            results.Add(0);
+            int j = i;
+            Thread thread = new(() => results[j] = RecursiveStep(numbers[j], 0, runs));
+            threads.Add(thread);
+            thread.Start();
+        }
+        foreach (Thread thread in threads)
+        {
+            thread.Join();
+        }
+        return results.Sum();
+    }
+
+    private static long RecursiveStep(long number, int step, int maxStep)
+    {
+        if (step == maxStep) return 1;
+        if (number == 0) return RecursiveStep(1, step + 1, maxStep);
+        if (number.ToString().Length % 2 != 0) return RecursiveStep(number * 2024, step + 1, maxStep);
+        string s = number.ToString();
+        return RecursiveStep(long.Parse(s[..(s.Length / 2)]), step + 1, maxStep) + RecursiveStep(long.Parse(s[(s.Length / 2)..]), step + 1, maxStep);
     }
 }
