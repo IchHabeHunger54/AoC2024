4904093ae7bba55558caa84975e302c6e56a200e
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-12-16 22:42:45 +0100

    day 16 task 2 part 2

--- AoC2024/Day16.cs
+++ AoC2024/Day16.cs
@@ -28,7 +28,7 @@ public class Day16() : Day(16)
                 }
             }
         }
-        return Dijkstra(positions.ToList(), start, end);
+        return Dijkstra(positions.ToList(), start, end, Direction.Right, out _);
     }
 
     protected override long TaskTwo(List<string> lines)
@@ -55,20 +55,24 @@ public class Day16() : Day(16)
                 }
             }
         }
-        List<List<Position>> dijkstraWithHistory = DijkstraWithHistory(positions.ToList(), start, end);
-        return dijkstraWithHistory.SelectMany(e => e).Distinct().Count();
+        return MultiDijkstra(positions.ToList(), start, end).Count;
     }
 
-    private static long Dijkstra(List<Position> input, Position start, Position end)
+    private static long Dijkstra(List<Position> input, Position start, Position end, Direction startDirection, out Direction direction)
     {
         Dictionary<Position, long> positions = InitDijkstra(input, start);
         LinkedList<PositionAndDirection> queue = new();
-        queue.AddFirst(new PositionAndDirection(start, Direction.Right));
+        queue.AddFirst(new PositionAndDirection(start, startDirection));
+        direction = startDirection;
         while (queue.Count > 0)
         {
             PositionAndDirection current = queue.First!.Value;
             queue.RemoveFirst();
-            if (current.Position == end) continue;
+            if (current.Position == end)
+            {
+                direction = current.Direction;
+                continue;
+            }
             Position front = current.Position + GetDirectionStep(current.Direction);
             if (input.Contains(front) && positions[front] >= positions[current.Position])
             {
@@ -91,46 +95,31 @@ public class Day16() : Day(16)
         return positions[end];
     }
 
-    private static List<List<Position>> DijkstraWithHistory(List<Position> input, Position start, Position end)
+    private static List<Position> MultiDijkstra(List<Position> input, Position start, Position end)
     {
-        Dictionary<Position, long> positions = InitDijkstra(input, start);
-        LinkedList<PositionAndDirection> queue = new();
-        queue.AddFirst(new PositionAndDirection(start, Direction.Right));
-        Dictionary<Position, List<Position>> history = new();
-        List<List<Position>> result = [];
-        while (queue.Count > 0)
+        HashSet<Position> result = [start, end];
+        long best = Dijkstra(input, start, end, Direction.Right, out _);
+        foreach (Position position in input)
         {
-            PositionAndDirection current = queue.First!.Value;
-            queue.RemoveFirst();
-            List<Position> currentHistory = history.GetValueOrDefault(current.Position, []);
-            if (current.Position == end)
+            if (position == start || position == end) continue;
+            long l1 = Dijkstra(input, start, position, Direction.Right, out Direction direction);
+            long l2 = Dijkstra(input, position, end, direction, out _);
+            if (l1 + l2 == best)
             {
-                result.Add([..currentHistory, current.Position]);
-                continue;
+                result.Add(position);
             }
-            Position front = current.Position + GetDirectionStep(current.Direction);
-            if (input.Contains(front) && positions[front] >= positions[current.Position])
+            // Heuristic: Only rerun if we're reasonably close.
+            else if (l1 + l2 + 3000 > best)
             {
-                queue.AddFirst(current with { Position = front });
-                positions[front] = Math.Min(positions[front], positions[current.Position] + 1);
-                history[front] = [..currentHistory, current.Position];
-            }
-            Position right = current.Position + GetDirectionStep(RotateClockwise(current.Direction));
-            if (input.Contains(right) && positions[right] >= positions[current.Position])
-            {
-                queue.AddLast(new PositionAndDirection(right, RotateClockwise(current.Direction)));
-                positions[right] = Math.Min(positions[right], positions[current.Position] + 1001);
-                history[right] = [..currentHistory, current.Position];
-            }
-            Position left = current.Position + GetDirectionStep(RotateCounterClockwise(current.Direction));
-            if (input.Contains(left) && positions[left] >= positions[current.Position])
-            {
-                queue.AddLast(new PositionAndDirection(left, RotateCounterClockwise(current.Direction)));
-                positions[left] = Math.Min(positions[left], positions[current.Position] + 1001);
-                history[left] = [..currentHistory, current.Position];
+                // Rerun the second Dijkstra for cases where we have a tile accessible from two sides.
+                l2 = Dijkstra(input, position, end, GetOpposite(direction), out _);
+                if (l1 + l2 == best)
+                {
+                    result.Add(position);
+                }
             }
         }
-        return result;
+        return result.ToList();
     }
 
     private static Position GetDirectionStep(Direction direction)
@@ -171,6 +160,17 @@ public class Day16() : Day(16)
             Direction.Down => Direction.Right,
             Direction.Left => Direction.Down,
             _ => throw new ArgumentOutOfRangeException(nameof(direction))
+        };
+    }
+
+    private static Direction GetOpposite(Direction direction)
+    {
+        return direction switch
+        {
+            Direction.Up => Direction.Down,
+            Direction.Down => Direction.Up,
+            Direction.Left => Direction.Right,
+            Direction.Right => Direction.Left
         };
     }
 
