e0bbf27b8a33a38a63fb6cb44df88f0880184908
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-12-17 14:21:31 +0100

    day 17 task 1

--- AoC2024/Day.cs
+++ AoC2024/Day.cs
@@ -10,12 +10,12 @@ public abstract class Day(int dayNumber)
 
     private static Day GetDay()
     {
-        return new Day16();
+        return new Day17();
     }
 
     private static void RunAll()
     {
-        Day[] days = [new Day1(), new Day2(), new Day3(), new Day4(), new Day5(), new Day6(), new Day7(), new Day8(), new Day9(), new Day10(), new Day11(), new Day12(), new Day13(), new Day14(), new Day15(), new Day16()];
+        Day[] days = [new Day1(), new Day2(), new Day3(), new Day4(), new Day5(), new Day6(), new Day7(), new Day8(), new Day9(), new Day10(), new Day11(), new Day12(), new Day13(), new Day14(), new Day15(), new Day16(), new Day17()];
         foreach (Day day in days)
         {
             day.Run();
---
+++ AoC2024/Day17.cs
@@ -0,0 +1,71 @@
+﻿namespace AoC2024;
+
+public class Day17() : Day(17)
+{
+    protected override long TaskOne(List<string> lines)
+    {
+        long a = long.Parse(lines[0].Replace("Register A: ", ""));
+        long b = long.Parse(lines[1].Replace("Register B: ", ""));
+        long c = long.Parse(lines[2].Replace("Register C: ", ""));
+        int[] program = lines[4].Replace("Program: ", "").Split(',').Select(int.Parse).ToArray();
+        int pointer = 0;
+        List<long> output = [];
+        while (pointer < program.Length)
+        {
+            int opcode = program[pointer];
+            int operand = program[pointer + 1];
+            long combo = operand switch
+            {
+                0 => 0,
+                1 => 1,
+                2 => 2,
+                3 => 3,
+                4 => a,
+                5 => b,
+                6 => c,
+                _ => throw new ArgumentOutOfRangeException(nameof(lines))
+            };
+            bool jump = false;
+            switch (opcode)
+            {
+                case 0:
+                    a = (long)(a / Math.Pow(2, combo));
+                    break;
+                case 1:
+                    b ^= operand;
+                    break;
+                case 2:
+                    b = combo % 8;
+                    break;
+                case 3:
+                    if (a == 0) break;
+                    pointer = operand;
+                    jump = true;
+                    break;
+                case 4:
+                    b ^= c;
+                    break;
+                case 5:
+                    output.Add(combo % 8);
+                    break;
+                case 6:
+                    b = (long)(a / Math.Pow(2, combo));
+                    break;
+                case 7:
+                    c = (long)(a / Math.Pow(2, combo));
+                    break;
+            }
+            if (!jump)
+            {
+                pointer += 2;
+            }
+        }
+        Console.WriteLine("String output: " + string.Join(',', output));
+        return 0;
+    }
+
+    protected override long TaskTwo(List<string> lines)
+    {
+        throw new NotImplementedException();
+    }
+}
