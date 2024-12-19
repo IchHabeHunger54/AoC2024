1ea0a827d8d35d301e6671479fcc1aad68c8dc3d
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-12-19 19:39:23 +0100

    day 19 task 2

--- AoC2024/Day19.cs
+++ AoC2024/Day19.cs
@@ -5,25 +5,22 @@ public class Day19() : Day(19)
     protected override long TaskOne(List<string> lines)
     {
         List<string> towels = lines[0].Split(", ").ToList();
-        Dictionary<string, bool> cache = new() { [""] = true };
-        return lines[2..].Select(e => HasPattern(e, towels, cache) ? 1 : 0).Sum();
+        Dictionary<string, long> cache = new() { [""] = 1 };
+        return lines[2..].Select(e => Math.Clamp(HasPattern(e, towels, cache), 0, 1)).Sum();
     }
 
-    private static bool HasPattern(string pattern, List<string> towels, Dictionary<string, bool> cache)
+    private static long HasPattern(string pattern, List<string> towels, Dictionary<string, long> cache)
     {
         if (cache.ContainsKey(pattern)) return cache[pattern];
         List<string> list = towels.Where(pattern.StartsWith).ToList();
-        if (list.Any(e => HasPattern(pattern[e.Length..], towels, cache)))
-        {
-            cache[pattern] = true;
-            return true;
-        }
-        cache[pattern] = false;
-        return false;
+        cache[pattern] = list.Select(e => HasPattern(pattern[e.Length..], towels, cache)).Sum();
+        return cache[pattern];
     }
 
     protected override long TaskTwo(List<string> lines)
     {
-        throw new NotImplementedException();
+        List<string> towels = lines[0].Split(", ").ToList();
+        Dictionary<string, long> cache = new() { [""] = 1 };
+        return lines[2..].Select(e => HasPattern(e, towels, cache)).Sum();
     }
 }
